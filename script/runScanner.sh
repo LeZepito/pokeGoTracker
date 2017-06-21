f3db98b0cdec93a07b8c7f8cc1554bc66fad829d
LeZepito <lezepito@gmail.com>
2017-06-21 20:16:42 +0200

    Removing ssl tracing from run command

--- script/runScanner.sh
+++ script/runScanner.sh
@@ -1,3 +1,3 @@
 [ -f pokeGoTracker.pid ] && echo "pokeGoTracker.pid already exists" && exit 1
 
-java -Djavax.net.ssl.debug=all -cp ../lib/json-20160212.jar:../lib/mail-1.4.7.jar:../bin/ ch/lezepito/pokeGoTracker/PokeGoTrackerScheduler & echo $! > pokeGoTracker.pid
+java -cp ../lib/json-20160212.jar:../lib/mail-1.4.7.jar:../bin/ ch/lezepito/pokeGoTracker/PokeGoTrackerScheduler & echo $! > pokeGoTracker.pid
